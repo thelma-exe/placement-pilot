f3e2481d3cb796c29a1b6846ada6f4c248a197a6
thelma-exe <thelmalohsainge@gmail.com>
2026-06-04 15:42:45 +0100

    Implement Axios GET request to retrieve raw HTML content from a URL

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 import axios from "axios";
+
+
+function getData () {
+    axios
+    .get("https://books.toscrape.com/")
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error));
+}
+
+getData();
